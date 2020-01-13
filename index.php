d86c80cc201b249ef8fc1b2b55cf93cc438b4c2f
antoningirard <antonin.girard.98@gmail.com>
2020-01-13 16:48:33 +0100

    img menu css equipe

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 
     <div class="bloc">
         <div class="bloc3">
-            <img class="portrait" src="assets/img/.jpg" alt="">
+            <img class="portrait" src="assets/img/personnes.jpg" alt="">
             <div class="presentation">
                 <h2>Pierre</h2>
-                <h3>Chef de Projet</h3>
+                <h3>Responsable de Projet</h3>
                 <ul class="lang">
                     <li class="material-icons">computer PHP</li>
                     <li class="material-icons">computer JQUERY</li>
