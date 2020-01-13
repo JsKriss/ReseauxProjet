159929f7bebd0446bcf0fe7e7cae0751ec4df27d
antoningirard <antonin.girard.98@gmail.com>
2020-01-13 14:44:00 +0100

    menu nav

--- inc/header.php
+++ inc/header.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php if(!empty($title)){echo $title;} else {echo 'nom du site';} ?></title>
-    <link rel="stylesheet" href="./asset/css/style.css">
 
-
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title></title>
 </head>
 <body>
 <header>
-    <nav>
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
-
-
-        </ul>
+    <nav class="topnav" id="myTopnav">
+        <a href="#home" class="active">Home</a>
+        <a href="#statistics">News</a>
+        <a href="#inscription">Contact</a>
+        <a href="#connexion">About</a>
+        <a href="#connexion">About</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
     </nav>
+    <div class="imageback">
+        <img class="imageback" src="assets/img/fond-menu.jpg" alt="">
+        <div class="bloc">
 
+        </div>
+    </div>
 </header>
+
