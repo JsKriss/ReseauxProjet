9e15fa3ba3cfa011a87b30810ef829f8dd2deb24
Pierre <pierre.halavent@gmail.com>
2020-01-15 12:05:18 +0100

    CSS du tableau de donnée Ajout d'une page 403

---
+++ 403.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>403</title>
+	<link rel="stylesheet" href="assets/css/403.css">
+  </head>
+  <body>
+    <div class="scene">
+  <div class="overlay"></div>
+  <div class="overlay"></div>
+  <div class="overlay"></div>
+  <div class="overlay"></div>
+  <span class="bg-403">403</span>
+  <div class="text">
+    <span class="hero-text"></span>
+    <span class="msg"><span>Accès</span> Interdit.</span>
+    <span class="support">
+      <span>innatendu?</span>
+      <a href="index.php">ACCUEIL</a>
+    </span>
+  </div>
+  <div class="lock"></div>
+</div>
+  </body>
+</html>
--- trame.php
+++ trame.php
@@ -1,4 +1,6 @@
+<link rel="stylesheet" href="assets/css/table.css">
 <?php
+require('inc/function.php');
 $fichier = file_get_contents('files/trameReseau.json');
 $json = json_decode($fichier, true);
 //    echo "<pre>";
@@ -7,65 +9,75 @@ $json = json_decode($fichier, true);
 
 require("inc/header.php"); ?>
 
+<div class="wrap-tableau">
+    <div class="container">
+        <div class="wrap-tableau2">
+            <div class="tableau">
+                <table id="tableau">
+                    <thead>
+                        <tr class="headtableau">
+                            <th>Date et heure</th>
+                            <th>Adresse IP Source</th>
+                            <th>Adresse IP Destination</th>
+                            <th>Adresse MAC Source</th>
+                            <th>Adresse MAC Destination</th>
+                            <th>Protocole</th>
+                            <th>Port Source</th>
+                            <th>Port Destination</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $count = count($json);
+                    //        if(is_logged()) {
+                    for ($i = 0; $i < $count; $i++) {
+                        echo '<tr>';
+                        $row = $json[$i]['_source']['layers'];
+                        if (isset($row['frame'])) {
+                            echo '<td>' . $json[$i]['_source']['layers']['frame']['frame.time'] . '</td>';
+                        } else {
+                            echo '<td></td>';
+                        }
+                        if (isset($row['ip'])) {
+                            echo '<td>' . $json[$i]['_source']['layers']['ip']['ip.src'] . '</td>';
+                            echo '<td>' . $json[$i]['_source']['layers']['ip']['ip.dst'] . '</td>';
+                        } else {
+                            echo '<td></td>';
+                            echo '<td></td>';
+                        }
+                        if (isset($row['eth'])) {
+                            echo '<td>' . $json[$i]['_source']['layers']['eth']['eth.src'] . '</td>';
+                            echo '<td>' . $json[$i]['_source']['layers']['eth']['eth.dst'] . '</td>';
+                        } else {
+                            echo '<td></td>';
+                            echo '<td></td>';
+                        }
+                        if (isset($row['udp'])) {
+                            echo '<td>UDP</td>';
+                            echo '<td>' . $json[$i]['_source']['layers']['udp']['udp.srcport'] . '</td>';
+                            echo '<td>' . $json[$i]['_source']['layers']['udp']['udp.dstport'] . '</td>';
+                        } else if (isset($row['tcp'])) {
+                            echo '<td>TCP</td>';
+                            echo '<td>' . $json[$i]['_source']['layers']['tcp']['tcp.srcport'] . '</td>';
+                            echo '<td>' . $json[$i]['_source']['layers']['tcp']['tcp.dstport'] . '</td>';
+                        } else {
+                            echo '<td></td>';
+                            echo '<td></td>';
+                            echo '<td></td>';
+                        }
+                        echo '</tr>';
+                    }
+                    //        } else {
+                    //            header('Location: 403.php');
+                    //        }
 
-    <table id="tableau">
-        <thead>
-        <th>Date et heure</th>
-        <th>Adresse IP Source</th>
-        <th>Adresse IP Destination</th>
-        <th>Adresse MAC Source</th>
-        <th>Adresse MAC Destination</th>
-        <th>Protocole</th>
-        <th>Port Source</th>
-        <th>Port Destination</th>
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 
-        </thead>
-        <tbody>
-        <?php
-        $nb = count($json);
-        $udp = 0;
-        $tcp = 0;
-        for ($i = 0; $i < $nb; $i++) {
-            echo '<tr>';
-            $row = $json[$i]['_source']['layers'];
-            if  (isset($row['frame'])){
-                echo '<td>' . $json[$i]['_source']['layers']['frame']['frame.time'] . '</td>';
-            } else {
-                echo '<td></td>';
-            }
-            if (isset($row['ip'])) {
-                echo '<td>' . $json[$i]['_source']['layers']['ip']['ip.src'] . '</td>';
-                echo '<td>' . $json[$i]['_source']['layers']['ip']['ip.dst'] . '</td>';
-            } else {
-                echo '<td></td>';
-                echo '<td></td>';
-            }
-            if (isset($row['eth'])) {
-                echo '<td>' . $json[$i]['_source']['layers']['eth']['eth.src'] . '</td>';
-                echo '<td>' . $json[$i]['_source']['layers']['eth']['eth.dst'] . '</td>';
-            } else {
-                echo '<td></td>';
-                echo '<td></td>';
-            }
-            if (isset($row['udp'])) {
-                echo '<td>UDP</td>';
-                echo '<td>' . $json[$i]['_source']['layers']['udp']['udp.srcport'] . '</td>';
-                echo '<td>' . $json[$i]['_source']['layers']['udp']['udp.dstport'] . '</td>';
-                $udp++;
-            }else if (isset($row['tcp'])) {
-                echo '<td>TCP</td>';
-                echo '<td>' . $json[$i]['_source']['layers']['tcp']['tcp.srcport'] . '</td>';
-                echo '<td>' . $json[$i]['_source']['layers']['tcp']['tcp.dstport'] . '</td>';
-                $tcp++;
-            } else {
-                echo '<td></td>';
-                echo '<td></td>';
-                echo '<td></td>';
-            }
-            echo '</tr>';
-        }
-        ?>
-        </tbody>
-    </table>
 
 <?php require("inc/footer.php");
