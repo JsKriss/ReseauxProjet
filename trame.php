65a6a4d9f7bea3e4b58354324465c45fce1733a9
Pierre <pierre.halavent@gmail.com>
2020-01-15 16:32:09 +0100

    session_start dans le header pour ne pas le recopier

--- trame.php
+++ trame.php
@@ -9,7 +9,7 @@ $json = json_decode($fichier, true);
 
 require("inc/header.php"); ?>
 
-<canvas id="myChart" width="400" height="400"></canvas>
+<canvas id="myChart"></canvas>
 
 <div class="wrap-tableau">
     <div class="container">
@@ -94,28 +94,24 @@ require("inc/header.php"); ?>
             labels: ['TCP', 'UDP'],
             datasets: [{
                 label: 'Types de connexions',
-                data: [<?=$tcp?>, <?=$udp?>],
+                data: [<?=$udp?>, <?=$tcp?>],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 165, 0, 0.2)',
                     'rgba(54, 162, 235, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 165, 0, 1)',
                     'rgba(54, 162, 235, 1)'
                 ],
                 borderWidth: 1
             }]
         },
         options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
+
         }
     });
+    chart.canvas.parentNode.style.height = '100px';
+    chart.canvas.parentNode.style.width = '100px';
 </script>
 
 
