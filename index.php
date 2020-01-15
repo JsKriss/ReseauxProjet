76473e327f5f8f1add38c3bc96b07339a12a2bfa
antoningirard <antonin.girard.98@gmail.com>
2020-01-15 09:25:52 +0100

    commit mercredi 15 matin

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php include('inc/header.php') ?>
-  <div class="bloc">
+<?php
+include ('inc/pdo.php');
+include('inc/function.php');
+include('inc/header.php');
+
+session_start();
+?>
+<div class="bloc">
     <div class="wrap">
         <div class="bloc3">
             <img class="portrait" src="assets/img/personnes.jpg" alt="">
@@ -12,6 +18,13 @@
                 </ul>
             </div>
         </div>
+        <div id="ex1" class="modal">
+            <p>Thanks for clicking. That felt good.</p>
+
+        </div>
+
+        <!-- Link to open the modal -->
+        <p><a href="#ex1" rel="modal:open">Open Modal</a></p>
         <div class="bloc1">
             <img class="portrait" src="assets/img/personnes.jpg" alt="">
             <div class="presentation">
@@ -46,5 +59,7 @@
             </div>
         </div>
     </div>
-  </div>
+    <div class="hexagone"><div class="hexagonemain"></div><span class="hexagonecontent"><img class="portrait" src="assets/img/personnes.jpg" alt="">Utque aegrum corpus quassari etiam levibus solet offensis, ita animus eius angustus et tener, quicquid increpuisset, ad salutis suae dispendium existimans factum aut cogitatum, insontium caedibus fecit victoriam luctuosam.</span></div>
+</div>
+
 <?php include('inc/footer.php') ?>
