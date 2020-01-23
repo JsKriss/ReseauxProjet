f1b869364fa19519947aa15354810d6dd192d67b
Le-Duude <57392459+Le-Duude@users.noreply.github.com>
2020-01-23 18:45:31 +0100

    placement des div footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,41 +1,43 @@
 <footer>
+    <div class="clear" ></divclass>
     <div class="footeur">
         <div class="wrap2">
-            <div class="bloc_foot">
+            <div class="bloc_foot socialnetwork">
                 <div class="sociaux">
-                    <div><a href="#"><img class="r_sociaux" src="assets/img/social_facebook_box_blue_256_30649.png"></a>
-                    </div>
+                    <div><a href="#"><img class="r_sociaux" src="assets/img/social_facebook_box_blue_256_30649.png"></a></div>
                     <div><a href="#"><img class="r_sociaux" src="assets/img/social-twitter-icon_34350.png"></a></div>
-                    <div><a href="#"><img class="r_sociaux" src="assets/img/instagram_socialnetwork_22374.png"></a>
-                    </div>
+                    <div><a href="#"><img class="r_sociaux" src="assets/img/instagram_socialnetwork_22374.png"></a></div>
                 </div>
             </div>
-            <div class="bloc_foot">
+
+            <div class="bloc_foot mentionslegales">
                 <p class="mentions">Lire nos <a href="mentions_legales.php">Mentions légales</a></p>
-                <p class="mentions"> ©2020 01copyright.fr. All right reserved.</p>
             </div>
-            <div class="bloc_foot">
+
+            <div class="bloc_foot raisonsocial">
                 <p class="infos">NetScan</p>
                 <p class="infos">24 place Saint-Marc, 76000 ROUEN</p>
             </div>
-            <div class="bloc_foot">
+
+            <div class="bloc_foot logonetscan">
                 <div><img class="logo3" src="assets/img/logo.png"></div>
             </div>
         </div>
     </div>
+    <div id="copyright"><p class="mentions"> ©2020 01copyright.fr. All right reserved.</p></div>
 
     <script
-            src="https://code.jquery.com/jquery-2.2.4.min.js"
-            integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-            crossorigin="anonymous"></script>
+        src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+        crossorigin="anonymous"></script>
     <script
-            src="https://code.jquery.com/jquery-3.4.1.min.js"
-            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-            crossorigin="anonymous"></script>
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
     <script
-            src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-            integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
-            crossorigin="anonymous"></script>
+        src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+        integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+        crossorigin="anonymous"></script>
     <script src="assets/js/jquery.fittext.js"></script>
     <script src="assets/js/jquery.modal.js"></script>
     <script src="assets/js/jquery.modal.min.js"></script>
